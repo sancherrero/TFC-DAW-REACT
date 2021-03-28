0e65478117ac26018c7d8381fb97df85729dc869
zacht3000 <santiago.naranjo76@gmail.com>
2021-03-28 12:23:52 +0200

    Merge a las ramas

--- tfc-visual/src/components/containerFluid/ContainerFluid.js
+++ tfc-visual/src/components/containerFluid/ContainerFluid.js
@@ -5,11 +5,9 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NavBar from "../nav/NavBar";
 import Carrusel from "../carousel/Carousel";
-<<<<<<< HEAD
 import CardComponent from "../card/CardComponent";
 =======
 import CardDeck from "../ofertasDeck/CardDeck";
->>>>>>> 1162fa999261f9334c7fe9a6d81591db64e4669f
 import Footer from "../footer/Footer";
 class ContainerFluid extends React.Component {
   render() {
@@ -19,16 +17,13 @@ class ContainerFluid extends React.Component {
           <Col>
             <NavBar />
             <Carrusel />
-<<<<<<< HEAD
             <CardComponent />
-=======
             <div className="section-ofertas">
               <h1>Novedades</h1>
               <hr></hr>
               <CardDeck />
               <CardDeck />
             </div>
->>>>>>> 1162fa999261f9334c7fe9a6d81591db64e4669f
             <Footer />
           </Col>
         </Row>
