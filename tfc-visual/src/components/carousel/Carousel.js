fe68f22edf96b514d12b7cbc5e48d648893229c7
zacht3000 <santiago.naranjo76@gmail.com>
2021-04-07 23:54:13 +0200

    Actualización: -Se han cambiado las imagenes del carrusel. -Se han quitado los textos adicionales al carrusel.

--- tfc-visual/src/components/carousel/Carousel.js
+++ tfc-visual/src/components/carousel/Carousel.js
@@ -14,42 +14,26 @@ class Carrousel extends React.Component {
         <Carousel className="d-flex justify-content-center" >
           <Carousel.Item>
             <img className="d-block" 
-              src="./img/Wallpaper_1.png?text=First"
+              src="./img/Banner_1.png?text=First"
               alt="First slide" 
               style={style}
             />
-            <Carousel.Caption>
-              <h3>¡¡Equipa tu ordenador a tu gusto!!</h3>
-              <p>En tú primer registro te damos un 15% en tu nuevo PC</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block"
-              src="./img/Wallpaper_1.png?text=Second"
+              src="./img/Banner_2.png?text=Second"
               alt="Second slide"
               style={style}
             />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block"
-              src="./img/Wallpaper_1.png?text=Third"
+              src="./img/Banner_3.png?text=Third"
               alt="Third slide"
               style={style}
             />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
     
