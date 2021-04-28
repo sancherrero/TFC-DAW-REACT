18722ba7559f3309b33e9e59e3db4c12bb394cf4
Santiago Naranjo Marcillo <snaranjo@teralco.com>
2021-04-28 15:10:01 +0200

    Actualización: - Añadido desplegable al componente card.

--- tfc-visual/src/components/ofertasDeck/Card.js
+++ tfc-visual/src/components/ofertasDeck/Card.js
@@ -1,53 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Route from "./../Route";
-import Link from "./../Link";
+import Accordion from 'react-bootstrap/Accordion'
 import Componente from "./../paginas/Componente";
+import Button from 'react-bootstrap/Button'
+import Spinner from "react-bootstrap/Spinner";
+import Table from 'react-bootstrap/Table';
+
 
 const OfertasDeck = (props) => {
-  return (
-    <Card
-      style={{
-        height: "600px",
-        marginTop: "10%",
-      }}
-    >
-      <Card.Img
-        variant="top"
-        src={props.items.url_imagen}
-        style={{
-          width: "100%",
-          height: "300px",
-          maxWidth: "300px",
-          maxHeight: "300px",
-          margin: "auto",
-          display: "block",
-        }}
-      />
-      <Card.Body>
-        <Card.Title>{props.items.nombre}</Card.Title>
-        <Card.Text>Proveedor: {props.items.proveedor}</Card.Text>
-        <Card.Text>
-          Precio: {props.items.precio_minimo | props.items.precio_total}€
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        {props.items.url_articulo !== undefined ? (
-          <a href={props.items.url_articulo}>Ir al sitio</a>
-        ) : (
-          <Link
-            href={"/componente/" + props.items.id}
-            className="text-blue item"
-          >
-            Ver mas
-          </Link>
-        )}
-      </Card.Footer>
-      <Route path={"/componente/" + props.items.id}>
-        <Componente />
-      </Route>
-    </Card>
-  );
-};
+  const [data, setData] = useState([]);
 
-export default OfertasDeck;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await axios(
+          `https://proyecto-final-daw.000webhostapp.com/ajax/componentes.php?search=${props.items.nombre}`
+        );
+        setData(result.data);
+      } catch (error) {
+        setData(null);
+        console.log(Object.keys(error), error.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+
+    return (
+      <Card
+        style={{
+          height: "600px",
+          marginTop: "10%",
+        }}
+      >
+        <Card.Img
+          variant="top"
+          src={props.items.url_imagen}
+          style={{
+            width: "100%",
+            height: "300px",
+            maxWidth: "300px",
+            maxHeight: "300px",
+            margin: "auto",
+            display: "block",
+          }}
+        />
+        <Card.Body>
+          <Card.Title>{props.items.nombre}</Card.Title>
+          <Card.Text>Proveedor: {props.items.proveedor}</Card.Text>
+          <Card.Text>
+            Precio: {props.items.precio_minimo | props.items.precio_total}€
+        </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          {props.items.url_articulo !== undefined ? (
+            <a href={props.items.url_articulo}>Ir al sitio</a>
+          ) : (
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    Ver más
+              </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                  <Table striped bordered hover>
+                    <thead>
+                      <th>Proveedor</th>
+                      <th>Precio</th>
+                    </thead>
+                    <tbody>
+                    {data !== null ? (
+                      data.map((item) => (
+                        <tr>
+                          <td><a href={item.url_articulo}>{item.proveedor}</a></td>
+                          <td>{item.precio_total} €</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <Spinner animation="border" role="status">
+                        <span className="sr-only">Cargando...</span>
+                      </Spinner>
+                    )}
+                    </tbody>
+                    </Table>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          )}
+        </Card.Footer>
+        <Route path={"/componente/" + props.items.id}>
+          <Componente />
+        </Route>
+      </Card>
+    );
+  };
+
+  export default OfertasDeck;
